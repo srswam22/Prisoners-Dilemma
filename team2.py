4a84e7509d0652c7d40da6d6e37c51af70e4dd2e
Kevin <kwpledger@users.noreply.github.com>
2017-07-27 09:40:05 -0400

    changed strategy
    changed from "always collude" to "always collude except when severely punished"

--- team2.py
+++ team2.py
@@ -26,7 +26,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if len(my_history) == 0: # It's the first round; collude.
+        return 'c'
+    else:
+        if my_history[-1]=='c' and their_history[-1]=='b':
+            return 'b' # Betray if severely punished last time,
+        else:
+            return 'c' # otherwise collude.
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
