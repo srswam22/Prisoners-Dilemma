619b63197bb4fa3d60ce26353f35080618da41ab
Kevin <kwpledger@users.noreply.github.com>
2017-07-27 09:49:52 -0400

    changed team name
    team east-west

--- team2.py
+++ team2.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = "East-West" # Only 10 chars displayed.
+strategy_name = "I'm nice until you cross me."
+strategy_description = "We collude except when we've been severely punished."
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
